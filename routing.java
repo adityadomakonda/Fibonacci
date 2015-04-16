aef51b4a332e8385e998dbcc38646b658f797233
adityadomakonda <adityadomakonda@gmail.com>
2015-04-15 23:02:36 -0400

    final sub

--- routing.java
+++ routing.java
@@ -13,11 +13,14 @@ public class routing{
 		graph.populate(input_file);
 		graph.populate_ip(ip_file);
 		for(int i=0; i<graph.total_vertices; i++){
+			// Run Dijkstra's SSP from source ID i
 			graph.dijkstraSSP(i);
+			// Create the routing table for Vertex id i using the result of Dijkstra's
 			graph.createRoutingTable(i);
 		}		
 		long distance = graph.dijkstraSSP(source,destination);
 		System.out.println(distance);
+		// Routes a packet from source to destination and prints all the prefixes matched
 		graph.route(source, destination);
 	}
 }
